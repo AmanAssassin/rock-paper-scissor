9cce7b23833588b63ca8871a9298e4483e0db056
Aman Maurya <64511985+AmanAssassin@users.noreply.github.com>
2023-08-13 22:11:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# Rock Paper Scissors Game
+
+[Game Screenshot]
+![Alt text](<Screenshot (6).png>)
+## Description
+
+This is a simple Rock Paper Scissors game built using HTML, CSS, and JavaScript. It allows users to play against the computer and keeps track of their wins, losses, and ties.
+
+## Features
+
+- Choose between Rock, Paper, and Scissors.
+- Play against the computer and see the result.
+- Scoreboard to keep track of wins, losses, and ties.
+- Reset button to clear the scoreboard.
+
+## How to Play
+
+1. Open the `index.html` file in your web browser.
+2. Click on one of the three buttons to select your move: Rock, Paper, or Scissors.
+3. The computer will also make a move.
+4. The game will display the result of the round (Win, Lose, or Tie) and update the scoreboard.
+
+## Contributing
+
+Contributions are welcome! If you want to enhance the game or fix any issues, feel free to fork the repository, make your changes, and create a pull request.
+
+## Credits
+
+- Icons by [Flaticon](https://www.flaticon.com/)
+- Background image by [Unsplash](https://unsplash.com/)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+Made with ❤️ by [Aman maurya](https://github.com/AmanAssassin)
---
+++ script.js
@@ -0,0 +1,92 @@
+
+let score = JSON.parse(localStorage.getItem('score'))
+if(score == null){
+    score={
+        Win :0,
+        Loss : 0,
+        Tie : 0
+    }
+}
+
+updateScore();
+
+
+function playGame(playerMove){
+const computerMove = pickComputerMove();
+let result = '';
+
+if(playerMove === 'Scissor'){
+    if(computerMove === 'Rock'){
+        result = 'You Lose';
+    }
+    else if (computerMove === 'Paper'){
+        result='You Win';
+    }
+    else if(computerMove === 'Scissor'){
+        result = 'Tie';
+    }
+}
+else if(playerMove === 'Paper'){
+    if(computerMove === 'Rock'){
+        result = 'You Win';
+    }
+    else if (computerMove === 'Paper'){
+        result='Tie';
+    }
+    else if(computerMove === 'Scissor'){
+        result = 'You Lose';
+    }
+}
+else if(playerMove === 'Rock'){
+    if(computerMove === 'Rock'){
+        result = 'Tie';
+    }
+    else if (computerMove === 'Paper'){
+        result='You Lose';
+    }
+    else if(computerMove === 'Scissor'){
+        result = 'You Win';
+    }
+
+}
+if (result === 'You Win') {
+  score.Win += 1;
+} else if (result === 'You Lose') {
+  score.Loss += 1;
+} else if (result === 'Tie') {
+  score.Tie += 1;
+}
+
+localStorage.setItem('score' , JSON.stringify(score));
+
+document.querySelector('.js-result').innerHTML = result;
+
+document.querySelector('.js-moves').innerHTML = ` You
+<img src="images/${playerMove}-emoji.png" class="move-icon" >
+computer 
+<img src="images/${computerMove}-emoji.png" class="move-icon" >`;
+
+updateScore();
+}
+
+function updateScore(){
+    document.querySelector('.js-score').innerHTML =`Wins: ${score.Win}, Losses: ${score.Loss}, Ties: ${score.Tie} ` ;
+}
+
+function pickComputerMove(){
+const randomNumber = Math.random();
+let computerMove = '';
+
+
+if(randomNumber >=0 && randomNumber<1/3){
+    computerMove = 'Rock';
+}
+else if(randomNumber>=1/3 && randomNumber<2/3){
+    computerMove ='Paper';
+}
+else if(randomNumber>=2/3 && randomNumber<1 ){
+    computerMove ='Scissor';
+}
+
+return computerMove;
+}
